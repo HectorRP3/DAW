2890e874ce67405ef60e1bd0b1b1a521e9f19c4b
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-04-07 18:40:19 +0200

    Api Conjunta

---
+++ PhysioCareModificicion/models/patient.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+let patientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlenght: 3,
+    maxlenght: 50,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    require: true,
+    minlenght: 3,
+    maxlenght: 50,
+    trim: true,
+  },
+  birthDate: {
+    type: Date,
+    require: true,
+  },
+  address: {
+    type: String,
+    maxlenght: 100,
+    trim: true,
+  },
+  insuranceNumber: {
+    type: String,
+    require: true,
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9]{9}$/,
+      "El número de seguro debe tener 9 caracteres alfanuméricos",
+    ],
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+});
+let Patient = mongoose.model("patient", patientSchema);
+module.exports = Patient;
---
+++ PhysioCareModificicion/models/physio.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+let physioSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlenght: 2,
+    maxlenght: 50,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    require: true,
+    minlenght: 2,
+    maxlenght: 50,
+    trim: true,
+  },
+  specialty: {
+    type: String,
+    require: true,
+    enum: ["Sports", "Neurological", "Pediatric", "Geriatric", "Oncological"],
+  },
+  licenseNumber: {
+    type: String,
+    require: true,
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9]{8}$/,
+      "El número de seguro debe tener 9 caracteres alfanuméricos",
+    ],
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+});
+
+let Physio = mongoose.model("Physio", physioSchema);
+module.exports = Physio;
